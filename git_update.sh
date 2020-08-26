9e53c3f4093536b8199fe819541e337bc01ad772
navinagaraj <navinagaraj18@gmail.com>
2020-08-26 15:35:26 +0530

    Function pointer

--- git_update.sh
+++ git_update.sh
@@ -1,3 +1,10 @@
+/************************************************/
+/* Author	: Naveenkumar N                 */
+/* Date		: 26-08-2020                    */
+/* Filename	: git_update.sh                 */
+/* Description	: It used to push your reposit- */
+/*                ories                         */
+/************************************************/
 #!/bin/bash
 git add -A
 echo "Git commit purpose"
