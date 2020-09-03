7fabe3c535d6f953d8c8687b03cae1bfe8583db2
navinagaraj <navinagaraj18@gmail.com>
2020-09-03 15:55:22 +0530

    hi

--- git_update.sh
+++ git_update.sh
@@ -1,13 +1,25 @@
-/************************************************/
-/* Author	: Naveenkumar N                 */
-/* Date		: 26-08-2020                    */
-/* Filename	: git_update.sh                 */
-/* Description	: It used to push your reposit- */
-/*                ories                         */
-/************************************************/
+#/************************************************/
+#/* Author       : Naveenkumar N                 */
+#/* Date         : 03-09-2020                    */
+#/* Filename     : git_update.sh                 */
+#/* Description  : It used to push your reposit- */
+#/*                ories                         */
+#/************************************************/
+
 #!/bin/bash
-git add -A
-echo "Git commit purpose"
-read data
-git commit -m "$data"
-git push -u
+git status
+echo "Enter 1 git commit"
+echo "Enter 2 to exit"
+read data_re # Read data from user
+if [ 1 == $data_re ]
+then
+  git add -A  
+  echo "Make commit command"
+  read data		# Read a data form user
+  git commit -m "$data" # Make commit
+  git push -u 		# Push to master
+  git pull		# pull a repository
+
+else
+   exit
+fi
